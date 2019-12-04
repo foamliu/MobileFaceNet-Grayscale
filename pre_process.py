8da2dde0790542ed62730a2d50d21eee4a1e7c13
Yang Liu <liuyang12@focusmedia.cn>
2019-12-04 10:02:32 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     imgrec = recordio.MXIndexedRecordIO(path_imgidx, path_imgrec, 'r')
 
     samples = []
+    class_ids = set()
 
     # %% 1 ~ 5179510
     for i in tqdm(range(5179510)):
@@ -21,17 +22,23 @@ if __name__ == "__main__":
             header, s = recordio.unpack(imgrec.read_idx(i + 1))
             img = mx.image.imdecode(s).asnumpy()
             img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
-            label = int(header.label)
-            filename = '{}.png'.format(i)
+            label = int(header.label[0])
+            class_ids.add(label)
+            filename = '{}.jpg'.format(i)
             samples.append({'img': filename, 'label': label})
             filename = os.path.join(IMG_DIR, filename)
             cv.imwrite(filename, img)
         except KeyboardInterrupt:
             raise
-        except:
-            pass
+        except Exception as err:
+            print(err)
+            print(i)
 
     with open(pickle_file, 'wb') as file:
         pickle.dump(samples, file)
 
     print('num_samples: ' + str(len(samples)))
+
+    class_ids = list(class_ids)
+    print(len(class_ids))
+    print(max(class_ids))
