27c49c757ac62dc544b1e3bbf239f6baad00aa11
Yang Liu <liuyang12@focusmedia.cn>
2019-12-04 14:19:41 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -13,11 +13,9 @@ data_transforms = {
     'train': transforms.Compose([
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ]),
     'val': transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
 }
 
